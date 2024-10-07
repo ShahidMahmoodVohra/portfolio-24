6b6cfefe312dd9fe63be42a758135f58c3ad4d7f
ShahidMahmoodVohra <ssmvohra@gmail.com>
2024-10-07 17:27:16 +0500

    navbar at 600px

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
     // el: document.querySelector('.section_main,.second,.about,.subscribe'),
     // smooth: true
 // });
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.getElementById('menu-btn');
+    const navbar = document.querySelector('.navbar-mobile');
+  
+    menuBtn.addEventListener('click', function() {
+      navbar.classList.toggle('is-visible');
+    });
+  });
+  
